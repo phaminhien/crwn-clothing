d0ba4eaae0d0121ee204d67bfeaba2a58604aecb
Hien Pham <phaminhien@hotmail.com>
2020-08-30 14:43:33 +0700

    stripe backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HHWpFKDCd58shUH7ObhJssL7yZWjbIil2SrzVOwkc3dQReYdYMzK3OcKXttSXpJ9QwLbwAsGIzKDpsWWdjVPe0I00N83RiRAn';
 
     const onToken = token => {
-        alert('Payment Successful');
+        axios({ 
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment');
+        });
     }
 
     return (
